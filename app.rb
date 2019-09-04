40b4e07c05d0f1b08dbcff44d76781ddb5ab127a
jordanmoore753 <jordanmoore753@gmail.com>
2019-09-04 14:38:06 -0400

    Update calendar style

--- app.rb
+++ app.rb
@@ -1,182 +1,195 @@
-require "sinatra"
-require "sinatra/content_for"
-require "tilt/erubis"
+# frozen_string_literal: true
 
-require_relative "database_persistence"
+require 'sinatra'
+require 'sinatra/content_for'
+require 'tilt/erubis'
+
+require_relative 'database_persistence'
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
-  set :erb, :escape_html => true
+  set :erb, escape_html: true
 end
 
 configure(:development) do 
-  require "sinatra/reloader"
+  require 'sinatra/reloader'
   also_reload 'database_persistence.rb'
 end
 
 helpers do 
-	def convert_param_crop_name(name)
-		name.split('_').map(&:capitalize).join(' ')
-	end
+  def convert_param_crop_name(name)
+    name.split('_').map(&:capitalize).join(' ')
+  end
 
-	def convert_crop_name_img(name)
-		name.gsub(' ', '_')
-	end
+  def convert_crop_name_img(name)
+    name.gsub(' ', '_')
+  end
 
-	def convert_num_to_season(num)
-		case num
-		when 1 then "Spring"
-		when 2 then "Summer"
-		when 3 then "Fall"
-		end
-	end
+  def convert_num_to_season(num)
+    case num
+    when 1 then 'Spring'
+    when 2 then 'Summer'
+    when 3 then 'Fall'
+    end
+  end
 
-	def convert_season_to_num(season)
-		case season 
-		when 'spring' then 1
-		when 'summer' then 2
-		when 'fall' then 3
-		end
-	end
+  def convert_season_to_num(season)
+    case season 
+    when 'spring' then 1
+    when 'summer' then 2
+    when 'fall' then 3
+    end
+  end
 
-	def convert_crop_name_with_space(name)
-		name.split(' ').join('_')
-	end
+  def convert_crop_name_with_space(name)
+    name.split(' ').join('_')
+  end
 
-	def convert_number_to_string(num)
-		return "0" if num.nil? || num.zero?
-		num.to_s.reverse.scan(/\d{1,3}/).join(',').reverse
-	end
+  def convert_number_to_string(num)
+    return '0' if num.nil? || num.zero?
+    num.to_s.reverse.scan(/\d{1,3}/).join(',').reverse
+  end
 
-	def create_array_of_crop_values(tuple)
-		tuple.values
-	end
+  def create_array_of_crop_values(tuple)
+    tuple.values
+  end
 
-	def reject_non_season_crops(crops)
-		curr_season_int = nil
-		case @season 
-		when 'Spring' then curr_season_int = 1
-		when 'Summer' then curr_season_int = 2
-		when 'Fall' then curr_season_int = 3
-		end
+  def reject_non_season_crops(crops)
+    curr_season_int = nil
+    case @season 
+    when 'Spring' then curr_season_int = 1
+    when 'Summer' then curr_season_int = 2
+    when 'Fall' then curr_season_int = 3
+    end
 
-		crops.reject! { |tuple| tuple[:season] != curr_season_int }
-	end
+    crops.reject! { |tuple| tuple[:season] != curr_season_int }
+  end
 
-	def all_planted_crops_in_season
-		@storage.all_planted_crops.select do |tuple|
-			convert_num_to_season(tuple[:season_id]) == @season
-		end
-	end
+  def all_planted_crops_in_season
+    @storage.all_planted_crops.select do |tuple|
+      convert_num_to_season(tuple[:season_id]) == @season
+    end
+  end
 
-	def available_planting_days
-		days_with_harvests = @planted_crops.map do |tuple| 
-												   if convert_num_to_season(tuple[:season_id]) == @season
-												   	(tuple[:sub_harvests] + [tuple[:first_harvest]]).flatten
-												   end
-												 end.flatten
+  def available_planting_days
+    days_with_harvests = @planted_crops.map do |tuple| 
+                           if convert_num_to_season(tuple[:season_id]) == @season
+                            (tuple[:sub_harvests] + [tuple[:first_harvest]]).flatten
+                           end
+                         end.flatten
 
-		days_with_harvests.select do |int|
-			days_with_harvests.count(int) >= 4
-		end
+    days_with_harvests.select do |int|
+      days_with_harvests.count(int) >= 4
+    end
 
-		(1..28).to_a.reject { |int| days_with_harvests.include?(int) }
-	end
+    (1..28).to_a.reject { |int| days_with_harvests.include?(int) }
+  end
 
-	def return_crop_name(id)
-		@storage.single_crop_by_id(id)[:name]
-	end
+  def return_crop_name(id)
+    @storage.single_crop_by_id(id)[:name]
+  end
 
-	def return_crop_name_reduce_name(id)
-		name = @storage.single_crop_by_id(id)[:name]
-		name.length > 6 ? name[0...5] + "..." : name
-	end
+  def return_crop_name_reduce_name(id)
+    name = @storage.single_crop_by_id(id)[:name]
+    name.length > 6 ? name[0...5] + '...' : name
+  end
 
-	def return_crop_id(name)
-		@storage.single_crop(name)[:id]
-	end
+  def return_crop_id(name)
+    @storage.single_crop(name)[:id]
+  end
 
-	def return_crop_profit(id, amount)
-		sell_price = @storage.prices_single_crop(id)[0][:sell_price]
-		amount_per_pick = @storage.single_crop_by_id(id)[:produces]
-		sell_price * amount_per_pick * amount
-	end
+  def return_crop_profit(id, amount)
+    sell_price = @storage.prices_single_crop(id)[0][:sell_price]
+    amount_per_pick = @storage.single_crop_by_id(id)[:produces]
+    sell_price * amount_per_pick * amount
+  end
 
-	def return_seed_cost(id, amount)
-		seed_price = @storage.prices_single_crop(id)[0][:seed_price]
-		seed_price * amount
-	end
+  def return_seed_cost(id, amount)
+    seed_price = @storage.prices_single_crop(id)[0][:seed_price]
+    seed_price * amount
+  end
 
-	def return_total_crop_gross
-		return 0 if @planted_crops.size.zero?
+  def return_total_crop_gross
+    return 0 if @planted_crops.size.zero?
 
-		total = 0
-		@planted_crops.each do |t| 
-			gross = (return_crop_profit(t[:crop_id], t[:amount_planted]))
-			if !t[:first_harvest].zero? &&  !t[:sub_harvests][0].zero?
-				gross *= t[:sub_harvests].size + 1
-			elsif t[:first_harvest].zero?
-				gross = 0
-			end
-			total += gross
-		end
-		total
-	end
+    total = 0
 
-	def return_total_seed_cost
-		return 0 if @planted_crops.size.zero?
+    @planted_crops.each do |t| 
+      gross = (return_crop_profit(t[:crop_id], t[:amount_planted]))
+      if !t[:first_harvest].zero? && !t[:sub_harvests][0].zero?
+        gross *= t[:sub_harvests].size + 1
+      elsif t[:first_harvest].zero?
+        gross = 0
+      end
+      total += gross
+    end
+    
+    total
+  end
 
-		@planted_crops.map do |t|
-			return_seed_cost(t[:crop_id], t[:amount_planted])
-		end.reduce(:+)
-	end
+  def return_total_seed_cost
+    return 0 if @planted_crops.size.zero?
 
-	def return_season_id(name)
-		@storage.single_crop(name)[:season]
-	end
+    @planted_crops.map do |t|
+      return_seed_cost(t[:crop_id], t[:amount_planted])
+    end.reduce(:+)
+  end
 
-	def return_first_harvest(name, date)
-		days = @storage.single_crop(name)[:until_harvest]
-		first_harvest = days + date 
-		first_harvest > 28 ? 0 : first_harvest
-	end
+  def return_total_profit
+    convert_number_to_string(return_total_crop_gross - 
+                             return_total_seed_cost )
+  end
 
-	def return_sub_harvests(name, first_harvest)
-		regrow_int = @storage.single_crop(name)[:regrow]
-		return 0 if regrow_int.zero? || first_harvest.zero?
+  def return_season_id(name)
+    @storage.single_crop(name)[:season]
+  end
 
-		sub_harvests = []
+  def return_first_harvest(name, date)
+    days = @storage.single_crop(name)[:until_harvest]
+    first_harvest = days + date 
+    first_harvest > 28 ? 0 : first_harvest
+  end
 
-		until first_harvest >= 28
-			first_harvest += regrow_int
+  def return_sub_harvests(name, first_harvest)
+    regrow_int = @storage.single_crop(name)[:regrow]
+    return 0 if regrow_int.zero? || first_harvest.zero?
 
-			if first_harvest <= 28
-				sub_harvests << first_harvest
-			else
-				break
-			end
-		end
+    sub_harvests = []
 
-		sub_harvests
-	end
+    until first_harvest >= 28
+      first_harvest += regrow_int
 
-	def convert_sub_harvests_string(sub_harvests)
-		return "{NULL}" if sub_harvests[0].zero?
-		str = ""
-		sub_harvests.each { |num| str += "#{num}, " }
-		2.times { str[-1] = '' }
-		str.prepend("{")
-		str += "}"
-	end
+      if first_harvest <= 28
+        sub_harvests << first_harvest
+      else
+        break
+      end
+    end
 
-	def add_planted_crop_to_db(str)
-		@storage.add_planted_crop(str)
-	end
+    sub_harvests
+  end
 
-	def remove_all_planted_crops(season)
-		@storage.delete_all_planted_crops_from_season(season)
-	end
+  def convert_sub_harvests_string(sub_harvests)
+    return "{NULL}" if sub_harvests[0].zero?
+    str = ""
+    sub_harvests.each { |num| str += "#{num}, " }
+    2.times { str[-1] = "" }
+    str.prepend("{")
+    str += "}"
+  end
+
+  def add_planted_crop_to_db(str)
+    @storage.add_planted_crop(str)
+  end
+
+  def remove_single_crop(id)
+    @storage.delete_single_planted_crop(id)
+  end
+
+  def remove_all_crops(season)
+    @storage.delete_all_planted_crops_from_season(season)
+  end
 end
 
 before do
@@ -188,62 +201,61 @@ after do
 end
 
 get "/" do
-	erb :index
+  erb :index
 end
 
 get "/crops/selection" do 
-	@current_crop = @storage.single_crop(params[:crop_name])
-	@prices = @storage.prices_single_crop(@current_crop[:id])[0]
-	@img_name = convert_crop_name_img(params[:crop_name])
-	erb :single_crop
+  @current_crop = @storage.single_crop(params[:crop_name])
+  @prices = @storage.prices_single_crop(@current_crop[:id])[0]
+  @img_name = convert_crop_name_img(params[:crop_name])
+  erb :single_crop
 end
 
 get "/calendar/:season" do
-	@season = params[:season].capitalize
-	@seasons = ["Spring", "Summer", "Fall"]
-	@planted_crops = all_planted_crops_in_season
-	@calendar_days = (1..28).to_a
-	@crops = reject_non_season_crops(@storage.all_crops)
-	@gross = return_total_crop_gross
-	@cost = return_total_seed_cost
-	erb :calendar
+  @season = params[:season].capitalize
+  @seasons = ["Spring", "Summer", "Fall"]
+  @planted_crops = all_planted_crops_in_season
+  @calendar_days = (1..28).to_a
+  @crops = reject_non_season_crops(@storage.all_crops)
+  @profit = return_total_profit
+  erb :calendar
 end
 
 get "/crop_directory" do
-	@crops = @storage.all_crops
-
-	erb :crop_directory
+  @crops = @storage.all_crops
+  erb :crop_directory
 end
 
 post "/add_crop_calendar" do 
-	param_hash = { crop: params["crop_name"],
-								 plant_date: params["plant_date"].to_i,
-								 amount: params["amount_planted"].to_i }
+  param_hash = { crop: params["crop_name"],
+                 plant_date: params["plant_date"].to_i,
+                 amount: params["amount_planted"].to_i }
 
-	crop_id = return_crop_id(param_hash[:crop])
-	season_id = return_season_id(param_hash[:crop])
-	season = convert_num_to_season(season_id)
-	first_harvest = return_first_harvest(param_hash[:crop], param_hash[:plant_date])
-	sub_harvest_array = return_sub_harvests(param_hash[:crop], first_harvest)
-	values = [crop_id, season_id, param_hash[:plant_date],
-						first_harvest, convert_sub_harvests_string(sub_harvest_array),
-						param_hash[:amount]]
+  crop_id = return_crop_id(param_hash[:crop])
+  season_id = return_season_id(param_hash[:crop])
+  season = convert_num_to_season(season_id)
+  first_harvest = return_first_harvest(param_hash[:crop], param_hash[:plant_date])
+  sub_harvest_array = return_sub_harvests(param_hash[:crop], first_harvest)
+  
+  values = [crop_id, 
+            season_id, 
+            param_hash[:plant_date],
+            first_harvest, 
+            convert_sub_harvests_string(sub_harvest_array),
+            param_hash[:amount]]
 
-	add_planted_crop_to_db(values)
-	redirect "/calendar/#{season.downcase}"
+  add_planted_crop_to_db(values)
+  redirect "/calendar/#{season.downcase}"
 end
 
 post "/:season/delete_single_crop" do
-	season = convert_season_to_num(params[:season]) 
-	id = params[:id]
-	@storage.delete_single_planted_crop(id)
-
-	redirect "/calendar/#{params[:season]}" 
+  id = params[:id]
+  remove_single_crop(id)
+  redirect "/calendar/#{params[:season]}"
 end
 
-post "/:season/delete_season_crops" do 
-	season = convert_season_to_num(params[:season].downcase)
-	@storage.delete_all_planted_crops_from_season(season)
-
-	redirect "/calendar/#{params[:season]}"
+post "/:season/delete_season_crops" do
+  season = convert_season_to_num(params[:season].downcase)
+  remove_all_crops(season)
+  redirect "/calendar/#{params[:season]}"
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -1,58 +1,120 @@
-ENV["RACK_ENV"] = "test"
+# frozen_string_literal: true
 
-require "pg"
-require "minitest/autorun"
-require "rack/test"
+ENV['RACK_ENV'] = 'test'
 
-require_relative "../app"
-require_relative "../database_persistence"
+require 'pg'
+require 'minitest/autorun'
+require 'rack/test'
 
+require_relative '../app'
+require_relative '../database_persistence'
+
+# This is the testing class.
 class AppTest < Minitest::Test
-	include Rack::Test::Methods
+  include Rack::Test::Methods
 
-	def app
-		Sinatra::Application
-	end
+  def app
+    Sinatra::Application
+  end
 
-	def session
-		last_request.env["rack.session"]
-	end
+  def session
+    last_request.env['rack.session']
+  end
 
-	def test_single_crop
-		get "/crops/selection?crop_name=Blue+Jazz"
+  def test_single_crop
+    get '/crops/selection?crop_name=Blue+Jazz'
 
-		assert_includes last_response.body, "Blue Jazz"
-		assert_includes last_response.body, "Spring"
-	end
+    assert_includes last_response.body, 'Blue Jazz'
+    assert_includes last_response.body, 'Spring'
+  end
 
-	def test_crop_by_season
-		get "/calendar/fall"
+  def test_crop_fall
+    get '/calendar/fall'
 
-		assert_includes last_response.body, "<option>Amaranth</option>"
-		assert_includes last_response.body, "<option>Yam</option>"
+    assert_includes last_response.body, '<option>Amaranth</option>'
+    assert_includes last_response.body, '<option>Yam</option>'
+  end
 
-		get "/calendar/spring"
+  def test_crop_spring
+    get '/calendar/spring'
 
-		assert_includes last_response.body, "<option>Blue Jazz</option>"
-		assert_includes last_response.body, "<option>Tulip</option>"
+    assert_includes last_response.body, '<option>Blue Jazz</option>'
+    assert_includes last_response.body, '<option>Tulip</option>'
+  end
 
-		get "/calendar/summer"
+  def test_crop_summer
+    get '/calendar/summer'
 
-		assert_includes last_response.body, "<option>Blueberry</option>"
-		assert_includes last_response.body, "<option>Wheat</option>"
-	end
+    assert_includes last_response.body, '<option>Blueberry</option>'
+    assert_includes last_response.body, '<option>Wheat</option>'
+  end
 
-	def test_add_crop
-		@season = "fall"
-		remove_all_planted_crops(@season)
+  def test_add_crop
+    post '/fall/delete_season_crops'
 
-		post "/add_crop_calendar", params = { "crop_name" => "Amaranth",
-																					"plant_date" => "3"
-																					"amount" => "40" }
+    post '/add_crop_calendar', 'crop_name' => 'Amaranth',
+                               'plant_date' => '3',
+                               'amount_planted' => 40
 
-		get last_response["Location"]
+    get last_response['Location']
 
-		assert_includes last_response.body, "Amara... x40"
-		assert_includes last_response.body, "$6000"
-	end
+    assert_includes last_response.body, 'Amara...'
+    assert_includes last_response.body, 'x40'
+    assert_includes last_response.body, '$6000'
+
+    post '/fall/delete_season_crops'
+  end
+
+  def test_delete_single_crop
+    # unable to grab the parameter from the route
+  end
+
+  def test_delete_all_crops
+    post '/fall/delete_season_crops'
+
+    post '/add_crop_calendar', 'crop_name' => 'Amaranth',
+                               'plant_date' => '3',
+                               'amount_planted' => 40
+
+    get last_response['Location']
+
+    assert_includes last_response.body, 'Amara...'
+
+    post '/fall/delete_season_crops'
+
+    get '/calendar/fall'
+
+    refute_includes last_response.body, 'Amara...'
+  end
+
+  def test_profit
+    post '/fall/delete_season_crops'
+
+    post '/add_crop_calendar', 'crop_name' => 'Amaranth',
+                               'plant_date' => '3',
+                               'amount_planted' => 40
+
+    get last_response['Location']
+
+    assert_includes last_response.body, '$3,200'
+
+    post '/fall/delete_season_crops'
+  end
+
+  def test_index
+    get '/'
+
+    assert_includes last_response.body, 'Stardew Valley Crop Assistant'
+    assert_includes last_response.body, 'Crop Directory'
+    assert_includes last_response.body, 'Crop Planner'
+    assert_includes last_response.body, 'interactive calendar'
+  end
+
+  def test_crop_directory
+    get '/crop_directory'
+
+    assert_includes last_response.body, 'Crop Directory Search'
+    assert_includes last_response.body, 'Choose the plant'
+    assert_includes last_response.body, 'Return to Index'
+  end
 end
