f140f0fc3baa0c28d608f59d382dee89b6aaa445
jordanmoore753 <jordanmoore753@gmail.com>
2020-02-26 07:13:18 -0500

    rm files

--- schema_2.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO crops(name, season, until_harvest, regrow, produces)
-VALUES ('Sweet Gem Berry', 3, 24, NULL, 1);
-
-INSERT INTO crop_prices (crop_id, sell_price, seed_price)
-VALUES (35, 3000, 1000);
